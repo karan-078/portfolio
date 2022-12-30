31749801fcdb14af3570b48f0e015fb28ecefbd9
karan <karanbisht117@gmail.com>
2022-12-30 15:32:27 +0530

    intial commit

---
+++ portfolio/assets/js/js.js
@@ -0,0 +1,169 @@
+// nav- bar meanu opeaning
+
+const nav_menu = document.getElementById("nav-menu");
+const menu_show_btn = document.getElementById("nav-toggle");
+const menu_hide_btn = document.getElementById("menu_hide_btn");
+const show_menu = "show_menu";
+
+// console.log(menu_show_btn);
+// console.log(menu_hide_btn);
+// console.log(nav_menu);
+menu_show_btn.addEventListener("click", funShow);
+menu_hide_btn.addEventListener("click", funHide);
+
+function funShow() {
+  nav_menu.classList.add(show_menu);
+}
+
+function funHide() {
+  nav_menu.classList.remove(show_menu);
+}
+
+// ::::::--active menu li a--::::::::
+
+// get all section that have an id defind
+
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  // Get current scroll postion
+
+  let scrollY = window.pageYOffset;
+
+  // now we loop through sections to get height , top and id values for each
+
+  // console.log(sections);
+
+  /*
+- if our current scroll postion enters the space where current section on screen is , add-
+- to know which link nneed an active class we use sectionId variable we are getting while
+*/
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+
+    let sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+
+    // use for debuging
+
+    //   let myn = document.querySelector(".nav__menu a[href*="+ sectionId + "]")
+
+    //   console.log(myn)
+    //
+  });
+}
+
+// // ::::::slider::::::
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 1,
+  spaceBetween: 40,
+  effect: "coverflow",
+  grabCursor: true,
+  centereSlides: true,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  coverflowEffect: {
+    rotate: 0,
+    stretch: -40,
+    depth: 80,
+    modifier: 1,
+    slideShadows: false,
+  },
+
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    410: {
+      slidesPerView: 1.1,
+      spaceBetween: 20,
+      coverflowEffect: {
+        stretch: -20,
+      },
+    },
+
+    640: {
+      slidesPerView: 1.4,
+      spaceBetween: 20,
+      coverflowEffect: {
+        stretch: -20,
+      },
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      coverflowEffect: {
+        stretch: -20,
+      },
+    },
+
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      coverflowEffect: {
+        stretch: -30,
+      },
+    },
+  },
+});
+
+// parallex effect ----
+
+var scene = document.getElementById("scene");
+
+var parallax = new Parallax(scene);
+
+var scene = document.getElementById("scene2");
+var parallax = new Parallax(scene2);
+
+var scene = document.getElementById("scene3");
+var parallax = new Parallax(scene3);
+
+var scene = document.getElementById("scene4");
+var parallax = new Parallax(scene4);
+
+// animation
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 200,
+  reset: true,
+});
+
+// bottm
+sr.reveal(`.sr01`, { delay: 300, origin: "bottom" }),
+  sr.reveal(`.sr02`, { delay: 600, origin: "bottom" }),
+  sr.reveal(`.sr03`, { delay: 900, origin: "bottom" }),
+  sr.reveal(`.sr04`, { delay: 1200, origin: "bottom" }),
+  sr.reveal(`.sr05`, { delay: 1500, origin: "bottom" });
+
+// top
+sr.reveal(`.sr06`, { delay: 300, origin: "top" }),
+  sr.reveal(`.sr07`, { delay: 600, origin: "top" }),
+  sr.reveal(`.sr08`, { delay: 900, origin: "top" }),
+  sr.reveal(`.sr09`, { delay: 1200, origin: "top" }),
+  sr.reveal(`.sr010`, { delay: 1500, origin: "top" });
